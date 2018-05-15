c9eaea855c60146d0d24628b3b588c60fc646fae
Jonathan Hermsen <jonathanhermsen@gmail.com>
2018-05-14 20:15:22 -0500

    Rework service worker

--- serviceworker.js
+++ serviceworker.js
@@ -1,51 +1,78 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
 
-// cache HTML pages for offline use
+workbox.core.setCacheNameDetails({
+	prefix: "vin-decoder",
+	suffix: "v1",
+	precache: "cache",
+	runtime: "cache"
+});
+
+workbox.precaching.precacheAndRoute([
+	"./offline/decode.json"
+]);
+
+// cache HTML for offline use
 workbox.routing.registerRoute(
-	/\.(?:html|htm)$/,
-	workbox.strategies.networkFirst({
-		cacheName: "dynamic-resources",
+	/(\/|\.html|\.htm)$/,
+	workbox.strategies.networkFirst(),
+);
+
+// cache static content like JS and CSS for faster (or offline) use
+workbox.routing.registerRoute(
+	/.*\.(?:js|css)$/,
+	workbox.strategies.cacheFirst({
+		plugins: [
+			new workbox.cacheableResponse.Plugin({
+				statuses: [0, 200]
+			})
+		]
 	}),
 );
 
-// cache static content like JS and CSS for faster / offline use
-workbox.routing.registerRoute(
-	/\.(?:js|css)$/,
-	workbox.strategies.staleWhileRevalidate({
-		cacheName: "static-resources",
-	}),
-);
-
+// cache fonts for offline use
 workbox.routing.registerRoute(
 	new RegExp("https://fonts.(?:googleapis|gstatic).com/(.*)"),
-	workbox.strategies.cacheFirst({
-		cacheName: "static-fonts"
-	}),
+	workbox.strategies.cacheFirst(),
 );
 
 workbox.routing.registerRoute(
 	new RegExp("https://vpic.nhtsa.dot.gov/api/*"),
-	workbox.strategies.networkFirst({
-		cacheName: "decoded-VINs",
-		plugins: [
-			new workbox.expiration.Plugin({
-				maxAgeSeconds: 60 * 60 * 24, // Only cache for a day
-				maxEntries: 5 // Only cache 5 VINs
-			}),
-		]
-	})
+	({
+		event
+	}) => {
+		let routeHandler = workbox.strategies.networkFirst({
+			cacheName: "vin-decoder-cache-v1",
+			plugins: [
+				new workbox.cacheableResponse.Plugin({
+					statuses: [0, 200]
+				}),
+				new workbox.expiration.Plugin({
+					maxAgeSeconds: 60 * 60 * 24, // Only cache for a day
+					maxEntries: 20
+				}),
+			]
+		});
+
+		return routeHandler.handle({
+				event
+			})
+			.catch(() => {
+				caches.match("offline/decode.json");
+			});
+	}
 );
 
-// apply theme
-self.addEventListener("fetch", async (event) => {
-	let requestEvent = event.request;
+workbox.routing.registerRoute(
+	"https://cdn.jsdelivr.net/npm/bootswatch@4.1.1/dist/yeti/bootstrap.css",
+	function (event) {
+		console.log(event);
+		let routeHandler = workbox.strategies.cacheFirst();
 
-	if (event.request.url === "https://cdn.jsdelivr.net/npm/bootswatch@4.1.1/dist/yeti/bootstrap.css") {
-		requestEvent = new Request(event.request.url.replace("yeti", getTheme()));
+		let response = new Response(event.url.href.replace("yeti", getTheme()));
+
+		routeHandler.handle(response);
 	}
-
-	return event.respondWith(fetch(requestEvent));
-});
+);
 
 function getTheme() {
 	var userAgent = navigator.userAgent;
