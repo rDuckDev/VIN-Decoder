48bbcb60fcead267ebcbe3c7ac10be9c0e5cca85
Jonathan Hermsen <jonathanhermsen@gmail.com>
2018-05-15 22:26:34 -0500

    Use separate cache for API calls

--- serviceworker.js
+++ serviceworker.js
@@ -1,5 +1,6 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
 
+const apiCacheName = "vin-decoder-api-v1";
 workbox.core.setCacheNameDetails({
 	prefix: "vin-decoder",
 	suffix: "v1"
@@ -25,29 +26,19 @@ workbox.routing.registerRoute(
 	}),
 );
 
+const apiURL = new RegExp("https://vpic.nhtsa.dot.gov/api/*");
 workbox.routing.registerRoute(
-	new RegExp("https://vpic.nhtsa.dot.gov/api/*"),
-	({
-		event
-	}) => {
-		let routeHandler = workbox.strategies.networkFirst({
-			cacheName: "vin-decoder-cache-v1",
-			plugins: [
-				new workbox.cacheableResponse.Plugin({
-					statuses: [0, 200]
-				}),
-				new workbox.expiration.Plugin({
-					maxAgeSeconds: 60 * 60 * 24, // one day
-					maxEntries: 20
-				}),
-			]
-		});
-
-		return routeHandler.handle({
-				event
-			})
-			.catch(() => {
-				caches.match("offline/decode.json");
-			});
-	}
+	apiURL,
+	workbox.strategies.staleWhileRevalidate({
+		cacheName: apiCacheName,
+		plugins: [
+			new workbox.cacheableResponse.Plugin({
+				statuses: [0, 200]
+			}),
+			new workbox.expiration.Plugin({
+				maxEntries: 20,
+				maxAgeSeconds: 60 * 60 * 24 // one day
+			}),
+		]
+	})
 );
