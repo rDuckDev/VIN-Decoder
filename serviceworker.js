4494284cd3819bca76af2a366a28e83e91edc485
Jonathan Hermsen <jonathanhermsen@gmail.com>
2018-05-16 20:31:37 -0500

    Bump version

--- serviceworker.js
+++ serviceworker.js
@@ -7,8 +7,8 @@ workbox.core.setCacheNameDetails({
 });
 
 workbox.precaching.precache([
-	"/rDuckDev/VIN-Decoder/v1.0.3/",
-	"/rDuckDev/VIN-Decoder/v1.0.3/offline/decode.json"
+	"/rDuckDev/VIN-Decoder/v1.0.4/",
+	"/rDuckDev/VIN-Decoder/v1.0.4/offline/decode.json"
 ]);
 
 workbox.routing.registerRoute(
@@ -46,5 +46,5 @@ workbox.routing.registerRoute(
 
 workbox.routing.setCatchHandler(({event}) => {
 	// respond with "app offline" message when API cannot be reached
-	if (apiURL.test(event.request.url)) return caches.match("/rDuckDev/VIN-Decoder/v1.0.3/offline/decode.json");
+	if (apiURL.test(event.request.url)) return caches.match("/rDuckDev/VIN-Decoder/v1.0.4/offline/decode.json");
 });
