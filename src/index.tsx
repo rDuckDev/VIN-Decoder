6def92b00b091519e1eb01da70cdaba6edc04e37
rDuckDev <30515855+rDuckDev@users.noreply.github.com>
2022-03-16 10:23:00 -0500

    refactor: migrate the manifest and service worker

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import worker from './registerServiceWorker';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
@@ -14,3 +15,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+worker.register();
