21eb0c793a5c56639d7efe7cdf639fe5530a91cd
rDuckDev <30515855+rDuckDev@users.noreply.github.com>
2022-04-06 08:29:20 -0500

    feat: disable service worker (temp)

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import worker from './registerServiceWorker';
+//import worker from './registerServiceWorker';
 import reportWebVitals from './reportWebVitals';
 import VinContextProvider from './utils/providers/VinContextProvider';
 
@@ -18,4 +18,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-worker.register();
+//worker.register();
