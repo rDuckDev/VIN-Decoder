55301976056af6abc2ec37eb7ee5a718a977a385
rDuckDev <30515855+rDuckDev@users.noreply.github.com>
2022-04-06 08:28:49 -0500

    fix: remove console logs and backup files

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 function Footer() {
   return (
     <footer className='container text-center mt-3'>
-      {console.log(`Footer: render at ${new Date().getTime()}`)}
       <section className='border-top d-inline-block p-2'>
         rDuckDev © {new Date().getFullYear()} | All rights reserved
       </section>
